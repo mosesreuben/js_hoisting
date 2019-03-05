9b81a0eadfb44616fd11029d68687f25d231076a
Moses Reuben <46658412+mosesreuben@users.noreply.github.com>
2019-03-05 14:26:00 -0800

    Add files via upload

---
+++ js_hoisting.js
@@ -0,0 +1,86 @@
+//Practice reading JavaScript the same way as the interpreter reads it.
+
+console.log(hello);                                   
+var hello = 'world';                                 
+
+//undefined
+//undefined
+
+var needle = 'haystack';
+test();
+function test(){
+	var needle = 'magnet';
+	console.log(needle);
+}
+
+//"magnet"
+//undefined
+
+var brendan = 'super cool';
+function print(){
+	brendan = 'only okay';
+	console.log(brendan);
+}
+console.log(brendan);
+
+//"super cool"
+//undefined
+
+var food = 'chicken';
+console.log(food);
+eat();
+function eat(){
+	food = 'half-chicken';
+	console.log(food);
+	var food = 'gone';
+}
+
+//"chicken"
+//"half-chicken"
+//undefined
+
+mean();
+console.log(food);
+var mean = function() {
+	food = "chicken";
+	console.log(food);
+	var food = "fish";
+	console.log(food);
+}
+console.log(food);
+
+//"unexpected identifier"
+
+console.log(genre);
+var genre = "disco";
+rewind();
+function rewind() {
+	genre = "rock";
+	console.log(genre);
+	var genre = "r&b";
+	console.log(genre);
+}
+console.log(genre);
+
+//undefined
+//"rock"
+//"r&b"
+//"disco"
+//undefined
+
+dojo = "san jose";
+console.log(dojo);
+learn();
+function learn() {
+	dojo = "seattle";
+	console.log(dojo);
+	var dojo = "burbank";
+	console.log(dojo);
+}
+console.log(dojo);
+
+//san jose
+//seattle
+//burbank
+//san jose
+//undefined
